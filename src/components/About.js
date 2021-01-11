de0ee2a0acba94dfb533500312f7d743b2152c8b
Itamar Fayler <itamarfayler@Itamars-MacBook-Pro.local>
2021-01-11 19:51:20 +0200

    small patch

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactPlayer from "react-player";
 import "../myLayout.css";
 import "../ProjectsCards/projectcards.css";
-import profilepic from "../profilepic/profilepic.png";
+import profilepic from "../profilepic/profilepicAvatar.jpg";
 
 export default class About extends Component {
   render() {
